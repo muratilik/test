c9132cb7365ffc1cf1aa2c8143e8139f1a19e69e
erdemoflaz <erdemoflaz4@gmail.com>
2017-02-12 18:14:35 +0300

    test

---
+++ js/index.js
@@ -0,0 +1,11 @@
+(function() {
+  $(function() {
+    return $('[data-toggle]').on('click', function() {
+      var toggle;
+      toggle = $(this).addClass('active').attr('data-toggle');
+      $(this).siblings('[data-toggle]').removeClass('active');
+      return $('.surveys').removeClass('grid list').addClass(toggle);
+    });
+  });
+
+}).call(this);
